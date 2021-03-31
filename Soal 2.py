6b7957a3dfab9b438561de8c1f9ea72450dbecd8
Nadya Indah Arifin <nandyaindah8@gmail.com>
2021-04-01 00:47:32 +0700

    Tugas 5

---
+++ Soal 2.py
@@ -0,0 +1,16 @@
+nama = input("Masukkan nama anda: ")
+nilai = int(input("Masukkan nilai anda: "))
+if nilai >= 85:
+    print("Halo", nama, "Nilai anda setelah dikonversi adalah A")
+elif nilai >= 84:
+    print("Halo", nama,"Nilai anda setelah dikonversi adalah A-")
+elif nilai >= 79:
+    print("Halo", nama, "Nilai anda setelah dikonversi adalah B+")
+elif nilai >= 74:
+    print("Halo", nama, "Nilai anda setelah dikonversi adalah B")
+elif nilai >= 69:
+    print("Halo", nama, "Nilai anda setelah dikonversi adalah C+")
+elif nilai >= 64:
+    print("Halo", nama, "Nilai anda setelah dikonversi adalah C")
+else:
+    print("Halo", nama, "Nilai anda setelah dikonversi adalah E")
---
+++ soal 1.py
@@ -0,0 +1,6 @@
+nama = input("Masukkan nama anda: ")
+jenis_kelamin = input("Masukkan jenis kelamin anda: ")
+if jenis_kelamin == "perempuan":
+    print("Selamat datang, Nyonya", nama)
+else:
+    print("Selamat datang, Tuan", nama)
